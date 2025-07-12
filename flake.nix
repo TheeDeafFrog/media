55736c11ed4540a12f67f7fb27175e46e38a15c2
kevin <36723751+TheeDeafFrog@users.noreply.github.com>
2025-07-12 06:51:49 +0200

    initial flake setup (broken)

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Home media server nixos system configuration";
+
+  inputs = {
+    # *** CHANGE HERE: Use the 25.05 stable branch ***
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    # Define a specific NixOS configuration for your machine.
+    # Replace 'your-hostname' with the actual hostname of your machine.
+    # You can find your hostname by running 'hostname' in your terminal.
+    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux"; # Adjust for your architecture
+
+      modules = [
+        ./nixos
+      ];
+    };
+  };
+}
